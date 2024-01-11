a56117500fbfabeef5bc17fd26a7f3770f0d7c6b
zenodeapp <zenodeapp@gmail.com>
2024-01-11 11:36:52 +0100

    Refactored and fixed upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,58 +4,98 @@ import (
 	"fmt"
 
 	sdkmath "cosmossdk.io/math"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	feemarketkeeper "github.com/evmos/ethermint/x/feemarket/keeper"
+	evmkeeper "github.com/evmos/ethermint/x/evm/keeper"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	ibcfeetypes "github.com/cosmos/ibc-go/v5/modules/apps/29-fee/types"
 	cronostypes "github.com/crypto-org-chain/cronos/x/cronos/types"
 )
 
-func (app *App) RegisterUpgradeHandlers(experimental bool) {
-	// upgrade handlers
-	planName := "plan_cronos"
-	planHandler := func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		m, err := app.mm.RunMigrations(ctx, app.configurator, fromVM)
-		if err != nil {
-			return m, err
-		}
+type UpgradeInfo struct {
+	Name    string
+	Info    string
+	Handler func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error)
+}
 
-		// Override feemarket parameters
-		params := app.FeeMarketKeeper.GetParams(ctx)
-		params.BaseFeeChangeDenominator = 300
-		params.ElasticityMultiplier = 4
-		params.BaseFee = sdk.NewInt(10000000000000)
-		params.MinGasPrice = sdk.NewDec(10000000000000)
-		app.FeeMarketKeeper.SetParams(ctx, params)
+// This is the fork upgrade to Cronos binaries
+func UpgradeV1(
+		mm *module.Manager,
+		configurator module.Configurator,
+		fm feemarketkeeper.Keeper,
+		evm *evmkeeper.Keeper,
+	) UpgradeInfo {
+	return UpgradeInfo{
+		Name: "plan_cronos",
+		Info: `'{"binaries":{"darwin/amd64":"","darwin/x86_64":"","linux/arm64":"","linux/amd64":"","windows/x86_64":""}}'`,
+		Handler: func(ctx sdk.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			m, err := mm.RunMigrations(ctx, configurator, fromVM)
+			if err != nil {
+				return m, err
+			}
+	
+			// Override feemarket parameters
+			fmParams := fm.GetParams(ctx)
+			fmParams.BaseFeeChangeDenominator = 300
+			fmParams.ElasticityMultiplier = 4
+			fmParams.BaseFee = sdk.NewInt(10000000000000)
+			fmParams.MinGasPrice = sdk.NewDec(10000000000000)
+			fm.SetParams(ctx, fmParams)
+	
+			// clear extra_eips from evm parameters
+			// Ref: https://github.com/crypto-org-chain/cronos/issues/755
+			evmParams := evm.GetParams(ctx)
+			evmParams.ExtraEIPs = []int64{}
+			zero := sdkmath.ZeroInt()
+			// fix the incorrect value on testnet parameters
+			evmParams.ChainConfig.LondonBlock = &zero
+			evm.SetParams(ctx, evmParams)
 
-		// clear extra_eips from evm parameters
-		// Ref: https://github.com/crypto-org-chain/cronos/issues/755
-		evmParams := app.EvmKeeper.GetParams(ctx)
-		evmParams.ExtraEIPs = []int64{}
-
-		// fix the incorrect value on testnet parameters
-		zero := sdkmath.ZeroInt()
-		evmParams.ChainConfig.LondonBlock = &zero
-
-		app.EvmKeeper.SetParams(ctx, evmParams)
-		return m, nil
+			return m, nil
+		},
 	}
-	app.UpgradeKeeper.SetUpgradeHandler(planName, planHandler)
+}
 
+func (app *App) RegisterUpgradeHandlers(experimental bool) {
+	upgradeV1 := UpgradeV1(
+		app.mm,
+		app.configurator,
+		app.FeeMarketKeeper,
+		app.EvmKeeper,
+	)
+
+	app.UpgradeKeeper.SetUpgradeHandler(
+		upgradeV1.Name,
+		upgradeV1.Handler,
+	)
+
+	// When a planned update height is reached, the old binary will panic
+	// writing on disk the height and name of the update that triggered it
+	// This will read that value, and execute the preparations for the upgrade.
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
 	if err != nil {
-		panic(fmt.Sprintf("failed to read upgrade info from disk %s", err))
+		panic(fmt.Errorf("failed to read upgrade info from disk: %w", err))
 	}
 
-	if !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		if upgradeInfo.Name == planName {
-			storeUpgrades := storetypes.StoreUpgrades{
-				Added: []string{ibcfeetypes.StoreKey, cronostypes.StoreKey},
-			}
+	if app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+		return
+	}
 
-			// configure store loader that checks if version == upgradeHeight and applies store upgrades
-			app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+	var storeUpgrades *storetypes.StoreUpgrades
+
+	switch upgradeInfo.Name {
+	case upgradeV1.Name:
+		storeUpgrades = &storetypes.StoreUpgrades{
+			Added: []string{ibcfeetypes.StoreKey, cronostypes.StoreKey},
 		}
+	default:
+		// no-op
+	}
+
+	if storeUpgrades != nil {
+		// configure store loader that checks if version == upgradeHeight and applies store upgrades
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, storeUpgrades))
 	}
 }
--- go.mod
+++ go.mod
@@ -30,7 +30,6 @@ require (
 	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.30.0
 	gopkg.in/yaml.v2 v2.4.0
-  github.com/zenodeapp/genesis-cronos v0.0.0-20240107083448-6b1dd5fedc2d432e0ea58834e000a06170310238
 )
 
 require (
