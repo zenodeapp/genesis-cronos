6b1dd5fedc2d432e0ea58834e000a06170310238
zenodeapp <zenodeapp@gmail.com>
2024-01-11 09:20:24 +0100

    Test upgrade folder for go module

---
+++ app/upgrades/v1/constants.go
@@ -0,0 +1,6 @@
+package v1
+
+const (
+	UpgradeName = "plan_cronos"
+	UpgradeInfo = `'{"binaries":{"darwin/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0/evmos_16.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0/evmos_16.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v16.0.0/evmos_16.0.0_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0/evmos_16.0.0_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0/evmos_16.0.0_Windows_x86_64.zip"}}'`
+)
