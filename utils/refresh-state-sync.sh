90244a10594d16d14477dfc3eafc1bd86de44f26
zenodeapp <zenodeapp@gmail.com>
2024-01-11 18:03:08 +0100

    Added utils

--- utils/refresh-state-sync.sh
+++ utils/refresh-state-sync.sh
@@ -4,6 +4,10 @@
 # Original: https://github.com/zenodeapp/restate-sync
 # ZENODE (https://zenode.app) - Adapted for GenesisL1
 
+# $1 is [height_interval] (default: 2000)
+# $2 is [rpc_server_1] (default: the first rpc configured in rpc_servers)
+# $3 is [rpc_server_2] (default: [rpc_server_1])
+
 # Root of the current repository
 REPO_ROOT=$(cd "$(dirname "$0")"/.. && pwd)
 
@@ -15,10 +19,7 @@ VERSION=main # Added versioning for non-breaking changes and security measures, 
 EXTERNAL_REPO=https://raw.githubusercontent.com/zenodeapp/restate-sync/$VERSION
 
 # restate-sync.sh
-# argument 1 is [height_interval] (default: 2000)
-# argument 2 is [rpc_server_1] (default: the first rpc configured in rpc_servers)
-# argument 3 is [rpc_server_2] (default: [rpc_server_1])
 # See https://github.com/zenodeapp/restate-sync/blob/main/restate-sync.sh
 curl -sO $EXTERNAL_REPO/restate-sync.sh
-sh ./restate-sync.sh "$BINARY_NAME" "$CONFIG_DIR" $1 $2 $3
+sh ./restate-sync.sh "$BINARY_NAME" "$NODE_DIR_NAME" $1 $2 $3
 rm restate-sync.sh
