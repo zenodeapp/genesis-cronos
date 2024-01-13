9f1d97757a5a457a1824f2e87d9c27e2354bd7e0
ZENODE <zenodeapp@gmail.com>
2024-01-13 16:54:56 +0100

    Update refresh-state-sync.sh
    Signed-off-by: ZENODE <zenodeapp@gmail.com>

--- utils/refresh-state-sync.sh
+++ utils/refresh-state-sync.sh
@@ -15,7 +15,7 @@ REPO_ROOT=$(cd "$(dirname "$0")"/.. && pwd)
 . "$REPO_ROOT/utils/_variables.sh"
 
 # Repository where the port shifter resides
-VERSION=main # Added versioning for non-breaking changes and security measures, TODO: add version instead of main
+VERSION=v1.0.0 # Added versioning for non-breaking changes and security measures, TODO: add version instead of main
 EXTERNAL_REPO=https://raw.githubusercontent.com/zenodeapp/restate-sync/$VERSION
 
 # restate-sync.sh
